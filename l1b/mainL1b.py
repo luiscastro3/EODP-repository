2bcf0a6e14fbb436437627d446b91e4307cadd71
luiscastro3 <lcastros903@gmail.com>
2026-09-10 19:29:13 +0200

    Update mainL1b.py

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\input"
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\myoutputs"
+auxdir = r'C:\\Users\\Luis Castro\\Documents\\GitHub\\EODP-repository\\auxiliary'
+indir = r"C:\\Users\\Luis Castro\\Downloads\\EODP\\EODP_TER_2021\\EODP-TS-L1B\\input"
+outdir = r"C:\\Users\\Luis Castro\\Downloads\\EODP\\EODP_TER_2021\\EODP-TS-L1B\\myOutput"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
