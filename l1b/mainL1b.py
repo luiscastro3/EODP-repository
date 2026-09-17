03eb3962415204e9325dd1f5286cb9a4a640db29
luiscastro3 <lcastros903@gmail.com>
2026-09-17 17:47:48 +0200

    Changes to l1bconfig and new output folder for not equalized data

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -6,7 +6,7 @@ from l1b.src.l1b import l1b
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\Luis Castro\\Documents\\GitHub\\EODP-repository\\auxiliary'
 indir = r"C:\\Users\\Luis Castro\\Downloads\\EODP\\EODP_TER_2021\\EODP-TS-L1B\\input"
-outdir = r"C:\\Users\\Luis Castro\\Downloads\\EODP\\EODP_TER_2021\\EODP-TS-L1B\\myOutput"
+outdir = r"C:\\Users\\Luis Castro\\Downloads\\EODP\\EODP_TER_2021\\EODP-TS-L1B\\myOutput_notEqualized"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
