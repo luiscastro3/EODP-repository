6177588c666b1f8952490e71b579f4e76a4a458e
luiscastro3 <lcastros903@gmail.com>
2026-09-24 20:08:50 +0200

    Implemented Smear and Motion MTFs and calculated the system MTF (Hsys)

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -69,7 +69,7 @@ class mtf:
 
         # Calculate the System MTF
         self.logger.debug("Calculation of the Sysmtem MTF by multiplying the different contributors")
-        Hsys = 1 # dummy
+        Hsys = Hdiff*Hdefoc*Hwfe*Hdet*Hsmear*Hmotion
 
         # Plot cuts ACT/ALT of the MTF
         self.plotMtf(Hdiff, Hdefoc, Hwfe, Hdet, Hsmear, Hmotion, Hsys, nlines, ncolumns, fnAct, fnAlt, directory, band)
@@ -188,6 +188,10 @@ class mtf:
         :return: Smearing MTF
         """
         #TODO
+
+        Hsmear_1d = np.sinc(ksmear*fnAlt)
+
+        Hsmear = np.tile(Hsmear_1d[:, np.newaxis], (1, ncolumns))
         return Hsmear
 
     def mtfMotion(self, fn2D, kmotion):
@@ -198,6 +202,8 @@ class mtf:
         :return: detector MTF
         """
         #TODO
+
+        Hmotion = np.sinc(fn2D*kmotion)
         return Hmotion
 
     def plotMtf(self,Hdiff, Hdefoc, Hwfe, Hdet, Hsmear, Hmotion, Hsys, nlines, ncolumns, fnAct, fnAlt, directory, band):
