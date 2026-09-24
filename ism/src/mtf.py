6645eb32f0722e7425ae40797670bdbacd954285
luiscastro3 <lcastros903@gmail.com>
2026-09-24 18:47:52 +0200

    Modified the freq2d function in mtf class

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -87,12 +87,28 @@ class mtf:
         :param focal: focal length [m]
         :param w: pixel size in meters [m]
         :return fn2D: normalised frequencies 2D (f/(1/w))
-        :return fr2D: relative frequencies 2D (f/(1/fc))
+        :return fr2D: relative frequencies 2D (f/fc)
         :return fnAct: 1D normalised frequencies 2D ACT (f/(1/w))
         :return fnAlt: 1D normalised frequencies 2D ALT (f/(1/w))
         """
         #TODO
 
+        fstepAlt = 1 / nlines / w
+        fstepAct = 1 / ncolumns / w
+
+        eps = 1e-6
+        fAlt = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAlt)
+        fAct = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAct)
+
+        [fAltxx, fActxx] = np.meshgrid(fAlt, fAct, indexing='ij')
+        f2D = np.sqrt(fAltxx * fAltxx + fActxx * fActxx)
+
+        fn2D = f2D/(1/w)
+        fnAlt = fAlt/(1/w)
+        fnAct = fAct/(1/w)
+
+        f_cutoff = D/(lambd*focal)
+        fr2D = f2D/f_cutoff
 
         return fn2D, fr2D, fnAct, fnAlt
 
